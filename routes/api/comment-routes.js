faa5bc8579e08ff28820d423070a46fa8c55b2de
Taylor Nyquist <tnyquist@gmail.com>
2020-10-04 23:04:16 -0500

    added reply and delete reply

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId> 
+// It's kind of like saying, "Go to this pizza, then look at this particular comment, then delete this one reply."
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
